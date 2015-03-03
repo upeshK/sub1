d0a803b2bd622abb281a22094ac9f98a7530110a
Upesh Kulatunge <Upesh.Kulatunge@ebuilder.com>
2015-03-03 15:47:56 +0530

    Add Hello World

---
+++ hello-world/src/main/java/com/test/GitMergeTest.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * Copyright 2014, eBuilder AB
+ * All rights reserved
+ * <p/>
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE
+ * of eBuilder AB;
+ * the contents of this file may not be disclosed to third parties,
+ * copied or duplicated in any form, in whole or in part,
+ * without the prior written permission of eBuilder AB.
+ * <p/>
+ * <p/>
+ * Author :Upesh Kulatunge<br> 
+ * Created: Oct 16, 2014<br>
+ */
+public class GitMergeTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Added to test branch");
+		
+	}
+
+}
---
+++ hello-world/src/main/java/com/test/Test.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author upesh
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Anypoint-1");
+System.out.println("Anypoint-2");
+System.out.println("Anypoint-3");
+System.out.println("Anypoint-4");
+System.out.println("Anypoint-5");
+	}
+
+}
